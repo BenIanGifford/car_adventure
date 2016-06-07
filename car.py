5832be639d9fb1275fe63a921e1a45f2b9dbbd7d
BenIanGifford <BenIanGifford@gmail.com>
2016-06-07 07:40:08 -0400

    Changed how the text parser works
    Changed how the text parser works to make it shorter.

--- car.py
+++ car.py
@@ -14,7 +14,7 @@ BOOL = [
     True,
     False]
 random.shuffle(EVENTS)
-user_command = input("Ignore")
+USER_COMMAND = input("Ignore")
 
 class Car():
     """Your car."""
@@ -37,10 +37,21 @@ class Car():
         if self.year < 2000:
             print("Your car is old")
 
+    def get_user_input(self):
+        USER_COMMAND = input("What do you want to do? ")
+
+        if USER_COMMAND == "refuel":
+            car.refuel()
+        elif USER_COMMAND == "drive":
+            car.drive()
+        elif USER_COMMAND == "specs":
+            car.specs()
+        elif USER_COMMAND == "loot":
+            car.loot()
+
 
     def refuel(self):
         """Fill up your gas."""
-        self.times_refueled = self.times_refueled + 1
 
         if self.fuel_level >= self.fuel_capacity:
             print("Your tank is already full :P")
@@ -51,25 +62,18 @@ class Car():
                 self.money = self.money -2
             print("Your fuel tank has been filled you have ", self.money, "Carmids left")
             self.fuel_level = self.fuel_capacity
+            self.times_refueled = self.times_refueled + 1
 
-        user_command = input("What do you want to do? ")
+        self.get_user_input()
 
-        if user_command == "refuel":
-            car.refuel()
-        elif user_command == "drive":
-            car.drive()
-        elif user_command == "specs":
-            car.specs()
-        elif user_command == "loot":
-            car.loot()
 
     def drive(self):
         """Drive your car"""
-        self.fuel_level = self.fuel_level -1
-        self.times_driven = self.times_driven + 1
-
-        if self.fuel_level > 1:
+        
+        if self.fuel_level > 0:
             print("Your car is moving")
+            self.times_driven = self.times_driven + 1
+            self.fuel_level = self.fuel_level -1
 
         if self.fuel_level <= self.fuel_capacity /2 and self.fuel_level > 0:
             print(self.fuel_level -1)
@@ -88,67 +92,49 @@ class Car():
         elif random.choice(EVENTS) != "bank":
             self.at_bank = False
 
-        user_command = input("What do you want to do? ")
-
-        if user_command == "refuel":
-            car.refuel()
-        elif user_command == "drive":
-            car.drive()
-        elif user_command == "specs":
-            car.specs()
-        elif user_command == "loot":
-            car.loot()
-
         if self.fuel_level <= 0 and self.at_gas_station == False:
             print("Game over, you collected", self.money, "Carmids drove",
             self.times_driven, "times refueled", self.times_refueled,
             "and looted", self.banks_looted, "banks")
+            
+        self.get_user_input()
+
 
     def specs(self):
         """Display specs of your car"""
         print(self.color, self.year, self.make, self.model,
          self.fuel_capacity, "Gallons", self.money, "Carmids")
-        user_command = input("What do you want to do? ")
-        if user_command == "refuel":
-           car.refuel()
-        elif user_command == "drive":
-            car.drive()
-        elif user_command == "specs":
-            car.specs()
-        elif user_command == "loot":
-            car.loot()
+        self.get_user_input()
 
     def loot(self):
         """Loot any bank you are at"""
-        self.banks_looted = self.banks_looted + 1
         if self.at_bank == True:
             if self.looted == True:
                 print("No more money here")
             elif random.choice(BOOL) == False and self.at_bank == True:
                 print("No money here")
+                self.banks_looted = self.banks_looted + 1
             elif self.at_bank == True and self.looted == False:
-                self.money = self.money + random.randrange(1,50)
+                self.money = self.money + random.randrange(1, 50)
                 print("You found some money, you now have", self.money, "Carmids")
+                self.banks_looted = self.banks_looted + 1
 
         if self.at_bank != True:
             self.at_bank = False
             print("you are not at a bank and therefore there is nothing to loot")
         self.looted = True
 
-        user_command = input("What do you want to do? ")
+        self.get_user_input()
 
-        if user_command == "refuel":
-            car.refuel()
-        elif user_command == "drive":
-            car.drive()
-        elif user_command == "specs":
-            car.specs()
-        elif user_command == "loot":
-            car.loot()
 
-car_type = input("What car do you want? Available cars are: pilot ")
+car_type = input("What car do you want? Available cars are: pilot and versa note ")
 if car_type == "pilot":
     car = Car("honda", "pilot", 2003, 19, "blue")
+    car.specs()
+    car.drive()
+elif car_type == "versa":
+    car = Car("nissan", "versa note", 2015, 10, "silver")
+    car.specs()
     car.drive()
 else:
     print("what?")
