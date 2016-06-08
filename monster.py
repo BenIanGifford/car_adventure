639bc95deac24b0cf665b7713c518279054d46f8
BenIanGifford <BenIanGifford@gmail.com>
2016-06-08 16:42:07 -0400

    v0.0.12

--- monster.py
+++ monster.py
@@ -1,7 +1,13 @@
 import random
 
 class Monster():
-  """The monster class"""
-  def __init__(self):
+
+    """The monster class"""
+
+    def __init__(self, name):
+        self.name = name
         self.health = random.randint(1, 25)
         self.damage = random.randint(1, 15)
+
+    def fight(self):
+        self.health = self.health - random.randint(1, 7)
