d07a48d1f29777123b665811709f333c7f479532
BenIanGifford <BenIanGifford@gmail.com>
2016-06-08 15:18:55 -0400

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,11 @@
+car_type = input("What car do you want? Available cars are: pilot and versa note ")
+if car_type == "pilot":
+    car = Car("honda", "pilot", 2003, 19, "blue")
+    car.specs()
+    car.drive()
+elif car_type == "versa":
+    car = Car("nissan", "versa note", 2015, 10, "silver")
+    car.specs()
+    car.drive()
+else:
+    print("what?")
